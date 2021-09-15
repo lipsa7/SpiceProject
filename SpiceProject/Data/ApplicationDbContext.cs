d03aeb2e544014017a533c754cae71623a78447a
lipsa7 <lipsakar98@gmail.com>
2021-09-15 13:36:47 +0530

    Add project files.

---
+++ SpiceProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SpiceProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpiceProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+        public DbSet<MenuItem> MenuItem { get; set; }
+        public DbSet<Coupon> Coupon { get; set; }
+
+
+    }
+}
---
+++ SpiceProject/Models/SubCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SpiceProject.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "SubCategory Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
